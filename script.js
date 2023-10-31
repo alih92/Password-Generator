07593753db9a2a179ba35b7bd6dbf396de716f1d
Ali Halavurt <alihalavurt@Alis-MBP.broadband>
2023-10-31 15:17:55 +0000

    prompt added to askfor password length and if statement to check password is a number and falls between 8-128

---
+++ script.js
@@ -0,0 +1,134 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var passwordLength = parseInt(prompt("Enter the length you would like your password to be: ")); // issue a prompt via a variable asking length of password. ParsenInt this prompt into a number as what user returns will be a string.
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength >128) {
+    alert("Password length incorrect! Password length must be a number between 8 and 128 characters.");
+  } //if statement to to check password length to make sure it is more than 8 and less than 128 and to ensure it is a number.
+//if length is more than 8 or less than 128 prompot user for the character types next
+//make a variable for each of the character types by using confirm to allow the user to choose which character they want. So for variables with confirms
+//
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+
+
+
+
+
+
+
+
